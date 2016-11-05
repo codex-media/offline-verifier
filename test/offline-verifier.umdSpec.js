056bb037ca4f10533f1a11c886b97db2d8902c6c
Roy Surjano <rsurjano@outlook.com>
2016-11-05 11:46:25 -0500

    updated mocha-test

--- test/offline-verifier.umdSpec.js
+++ test/offline-verifier.umdSpec.js
@@ -3,15 +3,25 @@ var expect = chai.expect;
 var jsdom = require("jsdom");
 global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
 global.window = document.defaultView;
-global.navigator = {userAgent: 'node.js'};
+global.navigator = { userAgent: 'node.js' };
 
 // library
-var OfflineVerifierCtor = require('../src/index').OfflineVerifier;
-var offlineVerifier = new OfflineVerifierCtor();
+var VerifierCtor = require('../src/index').Verifier;
+var offlineVerifier = new VerifierCtor();
 
-describe('starting', function () {
-    it('should start with connection enabled', function () {
+describe('starting', function() {
+    it('should start with connection enabled', function() {
         expect(offlineVerifier.status).to.be.true;
+        // var word = 'hello world';
+        // expect(word).to.equal('hello world');
+        // expect(word).to.not.equal('HELLO WORLD');
+        // expect(word).to.be.a('string');
+        // expect(word).to.not.be.a('number');
+        // expect(word).to.contain('hello');
+    })
+    it('should return current status', function() {
+        var currentStatus = offlineVerifier.getStatus();
+        expect(currentStatus).to.be.true;
         // var word = 'hello world';
         // expect(word).to.equal('hello world');
         // expect(word).to.not.equal('HELLO WORLD');
